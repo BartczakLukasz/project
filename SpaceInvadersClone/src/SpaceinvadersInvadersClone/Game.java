1f01adf398465a640c595648f0cee62af6b1e563
BartczakLukasz <bartczak94lukasz@gmail.com>
2017-02-27 21:40:52 +0100

    Add files via upload

---
+++ SpaceInvadersClone/src/SpaceinvadersInvadersClone/Game.java
@@ -0,0 +1,46 @@
+package SpaceinvadersInvadersClone;
+
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.image.BufferStrategy;
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Game extends Canvas {
+	private BufferStrategy strategy;
+	public Game() {
+		JFrame container = new JFrame("Space Invaders Clone");
+		JPanel panel = (JPanel) container.getContentPane();
+		panel.setPreferredSize(new Dimension(800,600));
+		panel.setLayout(null);
+		setBounds(0,0,800,600);
+		panel.add(this);
+		
+		setIgnoreRepaint(true);
+		
+		container.pack();
+		container.setResizable(false);
+		container.setVisible(true);
+		
+		container.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+		requestFocus();
+		createBufferStrategy(2);
+		strategy = getBufferStrategy();
+	}
+
+	public static void main(String argv[]) {
+		Game g =new Game();
+	}
+}
